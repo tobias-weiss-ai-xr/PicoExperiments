d77a6ceb2c2af60c35d623527ce1dd0cf57289df
Tobias Weiss <tobias@tobias-weiss.org>
2024-02-13 16:21:43 +0100

    ft experiments

--- Assets/Scripts/SensorTracking/FtManager.cs
+++ Assets/Scripts/SensorTracking/FtManager.cs
@@ -20,6 +20,7 @@ public unsafe class FtManager : MonoBehaviour
     public Transform TextParent;
 
     private List<TMP_Text> texts = new List<TMP_Text>();
+    private float[] blendShapeWeight = new float[72];
 
     private List<string> blendShapeList = new List<string>
     {
@@ -110,7 +111,9 @@ public unsafe class FtManager : MonoBehaviour
     private int rightLookInIndex;
     private int rightLookOutIndex;
 
-    private PxrFaceTrackingInfo faceTrackingInfo;
+    private FaceTrackingDataGetInfo info = new();
+
+    TrackingStateCode trackingState;
     private FileWriter fileWriter;
 
     void Start()
@@ -136,54 +139,73 @@ public unsafe class FtManager : MonoBehaviour
         fileWriter = GetComponent<FileWriter>();
         fileWriter.columnNames = Enumerable.Range(0, 72).Select(x => x.ToString()).ToList();
         fileWriter.StartLogging();
+        
+
+        // Want face tracking for the current app
+        trackingState = (TrackingStateCode)PXR_MotionTracking.WantFaceTrackingService();
+
+        // Query if the current device support face tracking
+        bool supported = false;
+        int supportedCount = 0;
+        FaceTrackingSupportedMode faceTrackingMode = FaceTrackingSupportedMode.PXR_FTM_FACE_LIPS_BS;
+        trackingState = (TrackingStateCode)PXR_MotionTracking.GetFaceTrackingSupported(ref supported, ref supportedCount, ref faceTrackingMode);
+
+        // Start face tracking
+        FaceTrackingStartInfo info = new FaceTrackingStartInfo();
+        info.mode = FaceTrackingSupportedMode.PXR_FTM_FACE;
+        trackingState = (TrackingStateCode)PXR_MotionTracking.StartFaceTracking(ref info);
     }
 
     // Update is called once per frame
     void Update()
     {
-#if !UNITY_EDITOR
         if (PXR_Plugin.System.UPxr_QueryDeviceAbilities(PxrDeviceAbilities.PxrTrackingModeFaceBit))
         {
-            switch (PXR_Manager.Instance.trackingMode)
-            {
-                case FaceTrackingMode.Hybrid:
-                    PXR_System.GetFaceTrackingData(0, GetDataType.PXR_GET_FACELIP_DATA, ref faceTrackingInfo);
-                    break;
-                case FaceTrackingMode.FaceOnly:
-                    PXR_System.GetFaceTrackingData(0, GetDataType.PXR_GET_FACE_DATA, ref faceTrackingInfo);
-                    break;
-                case FaceTrackingMode.LipsyncOnly:
-                    PXR_System.GetFaceTrackingData(0, GetDataType.PXR_GET_LIP_DATA, ref faceTrackingInfo);
-                    break;
-            }
-            fixed (float* ptr = faceTrackingInfo.blendShapeWeight)
-            {
-                for (int i = 0; i < 72; ++i)
-                {
-                    blendShapeWeights[i] = ptr[i];
-                    texts[i].text = $"{blendShapeList[i]}\n{(int)(ptr[i] * 120)}";
+            // switch (PXR_Manager.Instance.trackingMode)
+            // {
+            //     case FaceTrackingMode.Hybrid:
+            //         PXR_System.GetFaceTrackingData(0, GetDataType.PXR_GET_FACELIP_DATA, ref faceTrackingInfo);
+            //         break;
+            //     case FaceTrackingMode.FaceOnly:
+            //         PXR_System.GetFaceTrackingData(0, GetDataType.PXR_GET_FACE_DATA, ref faceTrackingInfo);
+            //         break;
+            //     case FaceTrackingMode.LipsyncOnly:
+            //         PXR_System.GetFaceTrackingData(0, GetDataType.PXR_GET_LIP_DATA, ref faceTrackingInfo);
+            //         break;
+            // }
 
-                    if (indexList[i] >= 0)
-                    {
-                        head.SetBlendShapeWeight(indexList[i], ptr[i]);
-                    }
+            // Get face tracking data
+            FaceTrackingDataGetInfo info = new FaceTrackingDataGetInfo();
+            info.displayTime = 0;
+            info.flags = FaceTrackingDataGetFlags.PXR_FACE_DEFAULT;
+            FaceTrackingData faceTrackingData = new FaceTrackingData();
+            float* b = stackalloc float[72]; // The array's length must by 72, otherwise the request will return an error
+            faceTrackingData.blendShapeWeight = b;
+
+            trackingState = (TrackingStateCode)PXR_MotionTracking.GetFaceTrackingData(ref info, ref faceTrackingData);
+
+            Debug.Log(trackingState);
+            for (int i = 0; i < 72; ++i)
+            {
+                texts[i].text = $"{blendShapeList[i]}\n{(int)(faceTrackingData.blendShapeWeight[i] * 120)}";
+
+                if (indexList[i] >= 0)
+                {
+                    head.SetBlendShapeWeight(indexList[i], 100 * faceTrackingData.blendShapeWeight[i]);
                 }
 
-                if (ComposedAvatar)
-                {
-                    teethBlendShape.SetBlendShapeWeight(tongueIndex, blendShapeWeights[51]);
-                    leftEyeExample.SetBlendShapeWeight(leftLookUpIndex, blendShapeWeights[31]);
-                    leftEyeExample.SetBlendShapeWeight(leftLookDownIndex, blendShapeWeights[0]);
-                    leftEyeExample.SetBlendShapeWeight(leftLookInIndex, blendShapeWeights[2]);
-                    leftEyeExample.SetBlendShapeWeight(leftLookOutIndex, blendShapeWeights[44]);
-                    rightEyeExample.SetBlendShapeWeight(rightLookUpIndex, blendShapeWeights[35]);
-                    rightEyeExample.SetBlendShapeWeight(rightLookDownIndex, blendShapeWeights[12]);
-                    rightEyeExample.SetBlendShapeWeight(rightLookInIndex, blendShapeWeights[11]);
-                    rightEyeExample.SetBlendShapeWeight(rightLookOutIndex, blendShapeWeights[45]);
-                }
+                // tongueBlendShape.SetBlendShapeWeight(tongueIndex, 100 * faceTrackingData.blendShapeWeight[51]);
+
+                leftEyeExample.SetBlendShapeWeight(leftLookUpIndex, 100 * faceTrackingData.blendShapeWeight[31]);
+                leftEyeExample.SetBlendShapeWeight(leftLookDownIndex, 100 * faceTrackingData.blendShapeWeight[0]);
+                leftEyeExample.SetBlendShapeWeight(leftLookInIndex, 100 * faceTrackingData.blendShapeWeight[2]);
+                leftEyeExample.SetBlendShapeWeight(leftLookOutIndex, 100 * faceTrackingData.blendShapeWeight[44]);
+                rightEyeExample.SetBlendShapeWeight(rightLookUpIndex, 100 * faceTrackingData.blendShapeWeight[35]);
+                rightEyeExample.SetBlendShapeWeight(rightLookDownIndex, 100 * faceTrackingData.blendShapeWeight[12]);
+                rightEyeExample.SetBlendShapeWeight(rightLookInIndex, 100 * faceTrackingData.blendShapeWeight[11]);
+                rightEyeExample.SetBlendShapeWeight(rightLookOutIndex, 100 * faceTrackingData.blendShapeWeight[45]);
             }
         }
-#endif
     }
     public void SendValues(string state)
     {
@@ -232,4 +254,29 @@ public unsafe class FtManager : MonoBehaviour
         if (DebugLogging) Debug.Log("Toggle debug view");
     }
 }
+// fixed (float* ptr = faceTrackingInfo.blendShapeWeight)
+// {
+//     for (int i = 0; i < 72; ++i)
+//     {
+//         blendShapeWeights[i] = ptr[i];
+//         texts[i].text = $"{blendShapeList[i]}\n{(int)(ptr[i] * 120)}";
 
+//         if (indexList[i] >= 0)
+//         {
+//             head.SetBlendShapeWeight(indexList[i], ptr[i]);
+//         }
+//     }
+
+//     if (ComposedAvatar)
+//     {
+//         teethBlendShape.SetBlendShapeWeight(tongueIndex, blendShapeWeights[51]);
+//         leftEyeExample.SetBlendShapeWeight(leftLookUpIndex, blendShapeWeights[31]);
+//         leftEyeExample.SetBlendShapeWeight(leftLookDownIndex, blendShapeWeights[0]);
+//         leftEyeExample.SetBlendShapeWeight(leftLookInIndex, blendShapeWeights[2]);
+//         leftEyeExample.SetBlendShapeWeight(leftLookOutIndex, blendShapeWeights[44]);
+//         rightEyeExample.SetBlendShapeWeight(rightLookUpIndex, blendShapeWeights[35]);
+//         rightEyeExample.SetBlendShapeWeight(rightLookDownIndex, blendShapeWeights[12]);
+//         rightEyeExample.SetBlendShapeWeight(rightLookInIndex, blendShapeWeights[11]);
+//         rightEyeExample.SetBlendShapeWeight(rightLookOutIndex, blendShapeWeights[45]);
+//     }
+// }
